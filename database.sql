3837f0b918b92f0dc42d7d51ed403c30a831fda1
KarpoVelho <40266114+KarpoVelho@users.noreply.github.com>
2021-08-30 21:01:09 +0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -13,7 +13,6 @@ CREATE TABLE IF NOT EXISTS `velhomyyjat` (
   `hinta` int(5) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---example
 /*!40000 ALTER TABLE `velhomyyjat` DISABLE KEYS */;
 INSERT IGNORE INTO `velhomyyjat` (`npc`, `jaljella`, `kaytossa`, `tuotot`, `x`, `y`, `z`, `h`, `tyyppi`, `tavara`, `label`, `hinta`) VALUES
 	(1, 3, 1, 0, 135.99000549316406, -57.11000061035156, 67.66999816894531, 67, 'ASE', 'WEAPON_PISTOL50', 'PISTOL.50', 10000),
