4384c703c8c7911e710ed212ba05674e3cd70ba2
KarpoVelho <40266114+KarpoVelho@users.noreply.github.com>
2021-08-30 20:15:08 +0300

    Create Config.lua

---
+++ Config.lua
@@ -0,0 +1,5 @@
+Config = {}
+Config.prosenttiosuus = 15 --Prosenttiosuus paljonko databaseen kertyy joka aseen/itemiin ostosta
+Config.ryostoaika = 5 --sekunteina kuinkauan myyjän ryöstö kestää
+Config.vaihdapaikkaa = true --vaihtaako npc paikkaa kun sen varastot loppuu?
+Config.paljonlisaa = 5  --monta itemiä lisaa varastoon jos on tyhjät ja vaihtanut paikkaa?
