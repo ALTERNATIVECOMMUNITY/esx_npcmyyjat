09270f7d9225eb093d964b95f8237e5f3e8d0276
KarpoVelho <40266114+KarpoVelho@users.noreply.github.com>
2021-09-01 23:04:18 +0300

    Update cl_main.lua

--- ck/cl_main.lua
+++ ck/cl_main.lua
@@ -8,23 +8,24 @@ local pedi
 CreateThread(function()
 	while ESX == nil do
 		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Wait(0)
+		Wait(10)
 	end
     TriggerServerEvent('velho_npcmyyjat:serverista')
     TriggerServerEvent('velho_npcmyyjat:dbsta')
-    Wait(5600)
+    Wait(5600) --venataan että saaadaaan sql info
     while true do
-    Wait(0)
+    local wait = 2500 --atm looppaa vaan joka 2,5 sec ettei mee liian raskaaks
     local coords = GetEntityCoords(PlayerPedId())
         for i=1, #database do
             for i=1, #npct do
-            if Vdist(database[i].x, database[i].y, database[i].z, coords.x, coords.y, coords.z) < 3.0 then 
+            if Vdist(database[i].x, database[i].y, database[i].z, coords.x, coords.y, coords.z) < 3.0 then
+                wait = 5  --nyt joka 5 tick/frame mikä vittu koska pitää renderöidä teksti
                 if not npct[i].pylly then
                     if not juttelee and not npct[i].ryostetaan then
                         teksti(database[i].x, database[i].y, database[i].z, tostring("~w~Paina ~g~E ~w~jutellaksesi!"))
                         if IsControlJustReleased(0, 38) then
                             TriggerServerEvent('velho_npcmyyjat:dbsta')
-                            Wait(500)
+                            Wait(500)--venataa et saadaan sqllst infot perille ennenku avataan menu
                             menu(i)
                             npct[i].juttelee = true
                         end
@@ -32,7 +33,7 @@ CreateThread(function()
                     if not npct[i].ryostetaan then
                         teksti(database[i].x, database[i].y, database[i].z, tostring("~w~Mitä sais olla?"))
                     else
-                        teksti(database[i].x, database[i].y, database[i].z, tostring("~r~Ryöstetään!!!"))
+                        teksti(database[i].x, database[i].y, database[i].z, tostring("~r~Ryöstetään!"))
                     end
                 end
             end
@@ -42,7 +43,7 @@ CreateThread(function()
                     ESX.UI.Menu.CloseAll()
                 end
             end
-            for i=1, #npct do
+            for i=1, #npct do --loopataan kaikki pedit jotka saadaan sql ja luodaan ne 
                 if not npct[i].check22 then
                     npct[i].check22 = true
                     luo(i)
@@ -50,6 +51,7 @@ CreateThread(function()
             end
         end
     end
+    Wait(wait) --ooämgee
     end
 end)
 
@@ -58,7 +60,7 @@ function menu(npc) --works
     local itemi = database[npc].tavara
     table.insert(elements, {label = 'Myyn: ' ..database[npc].label.. ' hintaan: $' ..database[npc].hinta, value = 1})
     table.insert(elements, {label = 'Minulla on vielä: ' ..database[npc].jaljella.. 'kpl jäljellä!', value = 2})
-    if IsPedArmed(PlayerPedId(), 7) then
+    if IsPedArmed(PlayerPedId(), 7) then --jos pelaajalla ase (lol?)
         table.insert(elements, {label = 'Ryöstä myyjä!', value = 3})
     end
     ESX.UI.Menu.Open(
@@ -93,7 +95,6 @@ function menu(npc) --works
             end
         end
         if data.current.value == 3 then
-	    ESX.UI.Menu.CloseAll()
             ryosto(npc)
         end
 	end,
@@ -107,40 +108,45 @@ end
 
 RegisterNetEvent('velho_npcmyyjat:poistaclient')
 AddEventHandler('velho_npcmyyjat:poistaclient', function(npc)
-    vaihdapaikkaa(npc)
+    vaihdapaikkaa(npc) 
 end)
 
 function luo(npc)
     npct[npc].luotu = false
-    local coords = GetEntityCoords(PlayerPedId())
-    if Vdist(database[npc].x, database[npc].y, database[npc].z, coords.x, coords.y, coords.z) < 150.0 then --npc luonti
-        if database[npc].kaytossa == 1 then
-            if not npct[npc].luotu then
-                RequestModel(GetHashKey(npct[npc].npcmodel))
-                while not HasModelLoaded(GetHashKey(npct[npc].npcmodel)) do
-                    Citizen.Wait(1)
-                end
-                    RequestAnimDict(npct[npc].anim)
-                    while not HasAnimDictLoaded(npct[npc].anim) do
-                        Citizen.Wait(1)
+    CreateThread(function()  
+        while true do --loopataan vain koska tarvitaan pelaajan kordinaatteja.
+            local wait2 = 2000 --looppaa joka 2sec ettei mee liian raskaaks (ei tarvi joka frame sillä ei renderöidä muuteskaa mitään lol)
+            local coords = GetEntityCoords(PlayerPedId())
+            if Vdist(database[npc].x, database[npc].y, database[npc].z, coords.x, coords.y, coords.z) < 150.0 then --jos pelaaja on lähel npct (siks koska fivem pedit poistuu jne iha vitu bugiset)
+                if database[npc].kaytossa == 1 then
+                    if not npct[npc].luotu then --jos npctä ei ole jo luotu
+                        RequestModel(GetHashKey(npct[npc].npcmodel))
+                        while not HasModelLoaded(GetHashKey(npct[npc].npcmodel)) do
+                            Citizen.Wait(1)
+                        end
+                        RequestAnimDict(npct[npc].anim)
+                        while not HasAnimDictLoaded(npct[npc].anim) do
+                            Citizen.Wait(1)
+                        end
+                        Wait(100)	
+                        pedi = CreatePed(4, GetHashKey(npct[npc].npcmodel), database[npc].x, database[npc].y, database[npc].z, database[npc].h, false, true)
+                        SetEntityHeading(pedi, database[npc].h+0.0)
+                        SetPedCanRagdollFromPlayerImpact(pedi, false)
+                        SetPedCanEvasiveDive(pedi, false)
+                        SetPedCanBeTargetted(pedi, false)
+                        SetEntityInvincible(pedi, true)
+                        SetBlockingOfNonTemporaryEvents(pedi, true)
+                        TaskPlayAnim(pedi, npct[npc].anim, npct[npc].anim2,1.0, 1.0, -1, 9, 1.0, 0, 0, 0)
+                        Wait(700)
+                        FreezeEntityPosition(pedi, true)
+                        npct[npc].luotu = true
+                        break --breakataan looppi koska ei oo tarvetta enää omg
                     end
-                Wait(100)	
-                pedi = CreatePed(4, GetHashKey(npct[npc].npcmodel), database[npc].x, database[npc].y, database[npc].z, database[npc].h, false, true)
-                SetEntityHeading(pedi, database[npc].h+0.0)
-                SetPedCanRagdollFromPlayerImpact(pedi, false)
-                SetPedCanEvasiveDive(pedi, false)
-                SetPedCanBeTargetted(pedi, false)
-                SetEntityInvincible(pedi, true)
-                SetBlockingOfNonTemporaryEvents(pedi, true)
-                TaskPlayAnim(pedi, npct[npc].anim, npct[npc].anim2,1.0, 1.0, -1, 9, 1.0, 0, 0, 0)
-                Wait(700)
-                FreezeEntityPosition(pedi, true)
-                npct[npc].luotu = true
+                end
             end
+            Wait(wait2)
         end
-    else
-        Wait(5000)
-    end
+    end)
 end
 
 
@@ -179,7 +185,6 @@ function ryosto(npc)
                     SetPedAsNoLongerNeeded(pedi)
                 end
                 Wait(Config.ryostoaika*1000)
-               -- TriggerServerEvent('velho_npcmyyjat:poistaped', npc)
                 Wait(1000)
                 npct[npc].pylly = true
                 TriggerServerEvent('velho_npcmyyjat:poistaped', npc)
@@ -209,8 +214,8 @@ end)
 
 function teksti(x, y, z, text)
     local onScreen,_x,_y=World3dToScreen2d(x,y,z+0.30)
-    local x2, y2, z2=table.unpack(GetGameplayCamCoords())
-    local dist = GetDistanceBetweenCoords(x2, y2, z2, x, y, z, 1)
+    local px,py,pz=table.unpack(GetGameplayCamCoords())
+    local dist = GetDistanceBetweenCoords(px,py,pz, x,y,z, 1)
     local scale = (1/dist)*1
     local fov = (1/GetGameplayCamFov())*100
     local scale = 1.0
@@ -228,7 +233,7 @@ function teksti(x, y, z, text)
         SetTextCentre(1)
         AddTextComponentString(text)
         DrawText(_x,_y)
-        local factor = (string.len(text)) / 370
+    local factor = (string.len(text)) / 370
     	DrawRect(_x,_y+0.0125, 0.013+ factor, 0.03, 0, 0, 0, 68)
     end
 end
